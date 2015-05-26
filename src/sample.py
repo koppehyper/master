505b467c62913b537aab2cf0db576923ce32a985
jenkins <koppehyper@gmail.com>
2015-05-26 02:40:07 +0000

    Fixed function

--- src/sample.py
+++ src/sample.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+memory = {}
+
 def f(x):
-    return 1 if x <= 1 else f(x-1) + f(x-2)
+    if str(x) in memory: return memory[str(x)]
+    memory[str(x)] = (1 if x <= 1 else f(x-1)+f(x-2))
+    return memory[str(x)]
 
 print 'Deploy test!'
-for x in range(1, 30):
-    print "f(%d) = %d" % (x, f(x))
+for x in range(0, 30):
+    print "f(%d) = %d" % (x+1, f(x+1))
 
 print 'Deploy end!'
