475c0cb0ddbace3b8490f20827e667eb6a103e35
Koppe Hyper <koppehyper@gmail.com>
2015-05-26 09:49:37 +0000

    Fix

--- src/test_tool.py
+++ src/test_tool.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-memory = {}
-
-def f(x):
-    if str(x) in memory: return memory[str(x)]
-    memory[str(x)] = (1 if x <= 1 else f(x-1)+f(x-2))
-    return memory[str(x)]
-
-print 'Deploy test!'
-
-with open('output.data') as fp: output_data = fp.read()
-
-print output_data
-
-for x in range(0, 30):
-    print "f(%d) = %d" % (x+1, f(x+1))
+with open('setting.conf') as s: lines = s.read()
 
 
-print 'Deploy end!'
+is_setting_ok = 'OK' in lines
+
+if is_setting_ok:
+    print 'Test passed!'
+else:
+    print 'Test FAILED!!!'
+
+exit(0 if is_setting_ok else 1)
