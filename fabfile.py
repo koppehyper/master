8908dbde2f455c5b0e3d198e5c331e6f0aba6d90
koppehyper <koppehyper@gmail.com>
2015-06-23 15:20:36 +0900

    take7

---
+++ fabfile.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from fabric.api import run, put, local, env, get, execute, settings, hide, task, abort
+from fabric.colors import *
+
+@task
+def test_task():
+    local('hostname')
+    green('Fabric on CircleCI')
+
