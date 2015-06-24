921774dba0393f286b0272dcf3a7722b45df7d4d
koppehyper <koppehyper@gmail.com>
2015-06-24 12:13:01 +0900

    test009

--- pr_merge.sh
+++
@@ -1,7 +0,0 @@
-git config --global user.name 'koppehyper'
-git config --global user.email 'koppehyper@gmail.com'
-
-./hub pull-request -m 'Test PullRequest'
-git checkout master
-git merge --no-ff ${CIRCLE_BRANCH}
-git push origin master
