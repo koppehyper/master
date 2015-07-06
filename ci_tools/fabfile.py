f8d5d1e153a9b5cedf1f7bf196c0c618ed227b5b
koppehyper <koppehyper@gmail.com>
2015-07-06 16:36:48 +0900

    stage026

--- ci_tools/fabfile.py
+++ ci_tools/fabfile.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from fabric.api import run, put, local, env, get, execute, settings, hide, task, abort, put
-from fabric.colors import *
-import os, requests
+from fabric.api import run
+from fabric.api import local
+from fabric.api import task
+from fabric.api import abort
+import os
+import requests
+
 
 def open_sg():
     sg_id = os.environ.get('MY_SECURITY_GROUP')
-    my_ip = local('dig +short myip.opendns.com @resolver1.opendns.com', capture=True)
-    local('aws ec2 authorize-security-group-ingress --group-id %s --protocol tcp --port 22 --cidr %s/32' 
-          % (sg_id, my_ip))
+    my_ip = local('dig +short myip.opendns.com @resolver1.opendns.com',
+                  capture=True)
+    local('aws ec2 authorize-security-group-ingress --group-id ' +
+          '%s --protocol tcp --port 22 --cidr %s/32' % (sg_id, my_ip))
+
 
 def close_sg():
     sg_id = os.environ.get('MY_SECURITY_GROUP')
-    my_ip = local('dig +short myip.opendns.com @resolver1.opendns.com', capture=True)
-    local('aws ec2 revoke-security-group-ingress --group-id %s --protocol tcp --port 22 --cidr %s/32'
-          % (sg_id, my_ip))
+    my_ip = local('dig +short myip.opendns.com @resolver1.opendns.com',
+                  capture=True)
+    local('aws ec2 revoke-security-group-ingress --group-id %s ' +
+          '--protocol tcp --port 22 --cidr %s/32' % (sg_id, my_ip))
+
 
 @task
 def test():
@@ -30,14 +38,16 @@ def test():
         abort('error!')
         slack(msg='Error! Please handle this problem!')
     finally:
-        close_sg() 
+        close_sg()
+
 
 def check_remote_branch():
     branch_list = local('git branch -a', capture=True)
     print branch_list
     branch = 'remotes/origin/%s' % os.environ.get('CIRCLE_BRANCH')
-    if not branch in branch_list:
+    if branch not in branch_list:
         raise ('Not Exist Branch : %s' % branch)
+
 
 @task
 def delete_branch():
@@ -52,7 +62,7 @@ def git_merge():
     local("./hub pull-request -m 'Test PullRequest'")
     local("git checkout master")
     local("git pull")
-    local("git merge --no-ff %s -m 'Merge master'" % circle_branch) 
+    local("git merge --no-ff %s -m 'Merge master'" % circle_branch)
     local("git push origin master")
     delete_branch()
 
@@ -63,24 +73,23 @@ def slack(msg=None):
     uri = "%s/%s" % (api_baseuri, method)
     token = os.environ.get('SLACK_TOKEN')
 
-    project_name = os.environ.get('CIRCLE_PROJECT_USERNAME') 
+    project_name = os.environ.get('CIRCLE_PROJECT_USERNAME')
     repo_name = os.environ.get('CIRCLE_PROJECT_REPONAME')
     build_no = os.environ.get('CIRCLE_BUILD_NUM')
 
     if not msg:
-        msg = 'MERGED > https://circleci.com/gh/%s/%s/%s' % (project_name, repo_name, build_no)
-    
+        msg = 'MERGED > https://circleci.com/gh/%s/%s/%s' % (
+            project_name, repo_name, build_no)
+
     channel = '#testroom'
     username = 'Hogehoge'
     icon_url = ':dora:'
 
-
     param = {"token": token,
-             "channel":channel,
-             "text":msg,
-             "username":username,
+             "channel": channel,
+             "text": msg,
+             "username": username,
              "link_names": True,
              "icon_emoji": icon_url}
 
-    res = requests.get(uri, params=param, verify=False)
-
+    requests.get(uri, params=param, verify=False)
